67d894414f1f384a53c3f7624a2a5a19da667344
Geetanjali Purohit <69380557+GeetanjaliP16@users.noreply.github.com>
2021-06-06 15:47:32 +0530

    MySql file

---
+++ final_with_descision.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2021 at 12:17 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arduino`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `final_with_descision`
+--
+
+CREATE TABLE `final_with_descision` (
+  `COL 1` varchar(10) DEFAULT NULL,
+  `COL 2` varchar(11) DEFAULT NULL,
+  `COL 3` varchar(137) DEFAULT NULL,
+  `COL 4` varchar(8) DEFAULT NULL,
+  `COL 5` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `final_with_descision`
+--
+
+INSERT INTO `final_with_descision` (`COL 1`, `COL 2`, `COL 3`, `COL 4`, `COL 5`) VALUES
+('id', 'polarity', 'clean', 'tweet', 'Decision'),
+('2622192321', '0.118181818', 'new twitter on one account and old twitter on another account?  when will it get to all accounts? #newtwitter', 'Positive', '0'),
+('2614182944', '0.136363636', 'cara make #newtwitter gimana?emang new twitter apaan ? -_-', 'Positive', '0'),
+('2677693643', '0.136363636', 'a review of the new twitter platform by intersearchmedia @collegedj  #newtwitter #college', 'Positive', '0'),
+('2671057189', '0.136363636', 'i refuse to click the #newtwitter button f you new twitter', 'Positive', '0'),
+('2673003946', '0.136363636', '#withoutmy #newtwitter i would be board like the rest of you with out the new twitter', 'Positive', '0'),
+('2629372558', '0.136363636', 'new twitter is the bigges dog #newtwitter', 'Positive', '0'),
+('2685076343', '0.136363636', 'making a new twitter background for the #newtwitter', 'Positive', '0'),
+('2658614093', '0.136363636', 'i\'m trying new twitter. for me it looks like this:  \Z\Z\Z\Z\Z\Z \Z\Z\Z\Z\Z \Z\Z\Z\Z\Z\Z\Z? #newtwitte', 'Positive', '0'),
+('2686732985', '0.136363636', 'everybody should get use to the #newtwitter cause they might make this new twitter permanent', 'Positive', '0'),
+('2659526706', '0.136363636', 'alright new twitter. #newtwitter', 'Positive', '0'),
+('2647279663', '0.136363636', 'no #newtwitter yet.  i am patiently waiting.  pssh, even @alqaeda gets the new twitter before me.', 'Positive', '0'),
+('2624104362', '0.136363636', 'why isnt mine the new twitter? #newtwitter', 'Positive', '0'),
+('2659641531', '0.136363636', 'the new @twitter promotional video. soundtrack by: will.i.am and nick minaj. \"check it out\".  [#newtwitter #socialmedia]', 'Positive', '0'),
+('2619530219', '0.136363636', 'quero new twitter #newtwitter', 'Positive', '0'),
+('2672082646', '0.136363636', 'damn you, new twitter, you just keep going and going. now i keep reading and reading without stopping for days and days. #newtwitter', 'Positive', '0'),
+('2603355408', '0.138257576', 'i just left the preview of new @twitter. couldn\'t stand it. especially the dms. when they force me on it, fine. #newtwitter', 'Positive', '0'),
+('2677896943', '0.140151515', 'be glad. rt @lizzieanderton i can not access the new twitter! :/ #newtwitter', 'Positive', '0'),
+('2658782972', '0.174545455', 'going back and forth from old twitter to new twitter i\'m astonished at how much better new twitter is. #newtwitter #ftw', 'Positive', '0'),
+('2707506357', '0.1875', '@nlschmidt i am actually a fan of #newtwitter but it took some time. golden ratio!', 'Positive', '0'),
+('2679836906', '0.211038961', '@support why are my tweets on new twitter not appearing in the #newtwitter timeline? is it because you know i am right?', 'Positive', '0'),
+('2706696190', '0.213068182', 'i was having that problem, too! rt @markgammon: rt @cdavid17: hey @twitter i can\'t create new lists!! #newtwitter #bugs', 'Positive', '0'),
+('2672050497', '0.213068182', 'al fin!! new twitter!! :d #newtwitter', 'Positive', '0'),
+('2707614487', '0.233333333', '#newtwitter looks quiet nice. so far it seems like an improvement.', 'Positive', '0'),
+('2648436723', '0.235227273', 'aww i want new twitter! i want #newtwitter @twitter xxxx', 'Positive', '0'),
+('2697513859', '0.25', 'really liking the #newtwitter!', 'Positive', '0'),
+('2707560275', '0.25', '... it opens the next \"page\" automatically as you reach the bottom of the page. :) #newtwitter', 'Positive', '0'),
+('2697905835', '0.284090909', '\"awesome\" is the word for #newtwitter. that is, if \"awesome\" was the new word for \"holy mother of god this is awful! it buuuuurns!\"', 'Positive', '0'),
+('2718119934', '0.284090909', '@biia_0509 la em cima tem escrito welcome to #newtwitter! read up on what?s new. you can also leave the preview and come back later. +', 'Positive', '0'),
+('2659156238', '0.318181818', 'love the new twitter. #newtwitter', 'Positive', '0'),
+('2613720093', '0.318181818', 'new twitter is more convenient. now i don\'t have to wait for a new page to load. :) #newtwitter', 'Positive', '0'),
+('2646971795', '0.318181818', 'i got the new twitter interface :) @newtwitter #newtwitter', 'Positive', '0'),
+('2639355734', '0.333333333', 'splodz blogz: quick blog yesterday about #newtwitter... any thoughts? ', 'Positive', '0'),
+('2616724065', '0.335227273', '@timeawhite a new twitter experience is coming! learn more about it? #newtwitter ez van oda irva', 'Positive', '0'),
+('2697451265', '0.35', '#newtwitter is cool... i guess', 'Positive', '0'),
+('2683272497', '0.356818182', 'well.. i like the new twitter! it makes all easier and more comfortable.. #newtwitter', 'Positive', '0'),
+('2647758636', '0.385227273', 'loving the new twitter! #newtwitter', 'Positive', '0'),
+('2612240190', '0.389393939', 'worldly things -> i love the new twitter.  love, love, love it.  thanks inventors of the #newtwitter', 'Positive', '0'),
+('2698082015', '0.4', '#newtwitter has got me using it from the website again. that definitely speaks for great redesign work.', 'Positive', '0'),
+('2617670220', '0.468181818', 'new twitter welcome screen prompt  #newtwitter', 'Positive', '0'),
+('2716689957', '0.468181818', 'dear @twitter.  i like #newtwitter, but it\'d be great if you could highlight/bold/change the messages link when i have a new dm. #kthxbye', 'Positive', '0'),
+('2697372254', '0.468181818', '@chrisohara10 let\'s make it official happy birthday coming live from @janinetoro\'s new twitter lol #newtwitter', 'Positive', '0'),
+('2697067527', '0.5', 'sooooooo, i think i like the #oldtwitter better than the #newtwitter', 'Positive', '0'),
+('2676789780', '0.568181818', 'my new twitter is twit awesome  #newtwitter', 'Positive', '0'),
+('2619956575', '0.568181818', 'whenever i tweet with new twitter... i feel great! *i luv the #newtwitter', 'Positive', '0'),
+('2707520260', '0.6', 'kind of liking #newtwitter.', 'Positive', '0'),
+('2716634840', '0.6', 'awesome. thanks. rt @jonpierce: w/e hack: adding classic-style rts to #newtwitter via chrome ', 'Positive', '0'),
+('2717485121', '0.7', '@abrasion @sarselack no reference to #newtwitter in my phone app, is there a good app that i could use on my laptop?', 'Positive', '0'),
+('2707665135', '0.75', 'i like to the #newtwitter.  it has better functions finally.  lol!', 'Positive', '0'),
+('2697983477', '-0.78125', 'i don\'t like the #newtwitter, this version is weird!!', 'Negative', '1'),
+('2707893267', '-0.75', 'arghhhh...esse #newtwitter ? mtooo chato! :(', 'Negative', '1'),
+('2697821842', '-0.7', 'fuck\'n #newtwitter you bad ass.!!!', 'Negative', '1'),
+('2718649841', '-0.65', 'is there not a url shortening service built into #newtwitter? am i blind? stupid? both?', 'Negative', '1'),
+('2717383858', '-0.625', 'shit! el #newtwitter es horrible!!', 'Negative', '1'),
+('2716533010', '-0.5', 'not trying #newtwitter again till the #fail phase is over.', 'Negative', '1'),
+('2616580673', '-0.30681818', '#newtwitter kya apa to emg\'e?? ga isa nyoba new twitter dr laptop~ =(', 'Negative', '1'),
+('2718115792', '-0.3', 'i like the #newtwitter but its slow', 'Negative', '1'),
+('2697708251', '-0.3', 'pq meu twitter t? no #newtwitter?  sucks...', 'Negative', '1'),
+('2658714519', '-0.25', 'how to customise your background for the #newtwitter  [blog] <-- i need to get round to this!', 'Negative', '1'),
+('2673387653', '-0.22121212', 'i\'m here on the #newtwitter. psh, i\'m going back i hate new twitter', 'Negative', '1'),
+('2603135146', '-0.21939393', 'the #newtwitter is so weird. seriously. why is there a new twitter? what was wrong with the old one? fix it now.', 'Negative', '1'),
+('2707082708', '-0.2125', 'my eyes hurt...and #newtwitter is confusing. one day it has \'reply\' and the other day it doesn\'t. how do i reply? how do i delete?', 'Negative', '1'),
+('2717847593', '-0.2', '#newtwitter - how can twitter send you traffic everyday?  -  ', 'Negative', '1'),
+('2657191913', '-0.17121212', 'i never got the new twitter yet :( rt @bdert: how can i get my old twitter back...the option to leave #newtwitter is gone...stuck...smh', 'Negative', '1'),
+('2718141943', '-0.125', 'i like the fact i don\'t have to use #newtwitter - it\'s actually pretty useless.', 'Negative', '1'),
+('2718684812', '-0.05', 'simultweeting harder with #newtwitter. have to open box to tweet. #bl10', 'Neutral', '0'),
+('2708101970', '0', 'to gostando do #newtwitter ... :d', 'Neutral', '0'),
+('2708237856', '0', 'what is going on with this #newtwitter? i have to click tweet twice in order to make 1 tweet.', 'Neutral', '0'),
+('2697948230', '0', 'wo0t finally got #newtwitter on my computer!', 'Neutral', '0'),
+('2706872761', '0', 'hmm, #newtwitter seems busted using 32-bit @opera 10.61 on linux. #sadboosh #guessillstickwithclients', 'Neutral', '0'),
+('2717480680', '0', 'como ve dm no #newtwitter ? ><', 'Neutral', '0'),
+('2697551741', '0', 'trying the #newtwitter', 'Neutral', '0'),
+('2697286739', '0', 'how the hell do you retweet on #newtwitter? it will not show up. help?', 'Neutral', '0'),
+('2716487229', '0', '#newtwitter is for #tittytuesday.', 'Neutral', '0'),
+('2717843704', '0', 'd0nt think i am liking the #newtwitter shyt #p00', 'Neutral', '0'),
+('2717728029', '0', 'if you don\'t like the #newtwitter, please sign this. ', 'Neutral', '0'),
+('2717139751', '0', 'does everybody have the #newtwitter now? everybody all caught up ?', 'Neutral', '0'),
+('2707381720', '0', 'to usando o #newtwitter  neem sei porque tbm \'-\'  kkkk', 'Neutral', '0'),
+('2707554195', '0', 'i\'m liking the #newtwitter if only for the fact that i no longer have to click the button to read earlier tweets ... //cont ...//', 'Neutral', '0'),
+('2708579861', '0', '#newtwitter motha suckas', 'Neutral', '0'),
+('2707316517', '0', 'am i the only one who likes the #newtwitter ?', 'Neutral', '0'),
+('2717465646', '0', 'finally #newtwitter for me!!!', 'Neutral', '0'),
+('2697213284', '0', '#newtwitter !!!', 'Neutral', '0'),
+('2707697552', '0.05', '@iambendavis #newtwitter is legit. yo are you busy next tuesday???', 'Neutral', '0'),
+('2717078769', '0.05', 'dr insult thinks #newtwitter is a complaint from one of his patients. #iphone #app  #free #comedy #humor #doctor', 'Neutral', '0'),
+('2659751039', '0.068181818', '~ omg ! finally the new version of twitter... now i can die [last tweet before the brand new twitter] #newtwitter', 'Neutral', '0'),
+('2658480663', '0.085227273', 'finally have new twitter! #yay #newtwitter', 'Neutral', '0');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
